32bd6a032e4b8df749c847a2216df0cc936e6296
Erick Fellipe <efrc22br@gmail.com>
2025-09-23 16:54:34 -0300

    Iniciado primeiros scripts do conversor

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,1 +1,38 @@
+const loading = document.querySelector(".loading-container");
+const convertBtn = document.getElementById("convertBtn");
+const message = loading.querySelector(".blinking");
+const spinner = loading.querySelector(".spinner");
+const moneyInput = document.getElementById("moneyInput");
+const finalResult = document.getElementById("result");
+
 const apiUrl = "https://api.exchangerate-api.com/v4/latest/USD";
+
+convertBtn.addEventListener("click", () => {
+  convertMoney();
+});
+
+function loadingConvert() {
+  loading.style.display = "block";
+}
+
+async function convertMoney() {
+  finalResult.style.color = "#000000";
+  finalResult.innerHTML = "";
+
+  if (moneyInput.value === "") {
+    finalResult.innerHTML = "<p>VALOR INVÁLIDO!</p>";
+    finalResult.style.color = "#ff0000";
+    return;
+  }
+
+  loadingConvert();
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+    spinner.style.display = "none";
+    message.innerText = "CONSEGUI!";
+  } catch (error) {
+    message.innerText = "ERRO! ❌";
+  }
+}
